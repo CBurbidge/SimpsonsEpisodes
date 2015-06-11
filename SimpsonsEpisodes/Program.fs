d34071c608efa155d7cf443ec66ed6edba830b5c
Chester Burbidge <chesterburbidge@gmail.com>
2015-06-11 22:48:25 +0100

    Get episodes rows

--- SimpsonsEpisodes/Program.fs
+++ SimpsonsEpisodes/Program.fs
@@ -1,7 +1,21 @@
-﻿// Learn more about F# at http://fsharp.net
-// See the 'F# Tutorial' project for more help.
+﻿open System
+open FSharp.Data
+
+//let simpsonsEpisodes = new HtmlProvider<"http://en.wikipedia.org/wiki/List_of_The_Simpsons_episodes">()
+
+let episodesUrl = "http://en.wikipedia.org/wiki/List_of_The_Simpsons_episodes"
+
 
 [<EntryPoint>]
 let main argv = 
-    printfn "%A" argv
+    let a  = 0
+    let episodes = HtmlDocument.Load(episodesUrl)
+    let trElements = episodes.Descendants["tr"]
+    let episodeRows: seq<HtmlNode> = 
+        trElements
+        |> Seq.filter(fun (e: HtmlNode) -> 
+            e.HasAttribute("class", "vevent")
+        )
+
+    Console.ReadKey() |> ignore
     0 // return an integer exit code
