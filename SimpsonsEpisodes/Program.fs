7b6c53762bad0d8d366b49d54602f4988389b5b2
Chester Burbidge <chesterburbidge@gmail.com>
2015-06-12 00:07:13 +0100

    Downloads to disk

--- SimpsonsEpisodes/Program.fs
+++ SimpsonsEpisodes/Program.fs
@@ -1,21 +1,37 @@
 ﻿open System
+open System.IO
+open FSharp.Configuration
 open FSharp.Data
 
-//let simpsonsEpisodes = new HtmlProvider<"http://en.wikipedia.org/wiki/List_of_The_Simpsons_episodes">()
+//type Settings = AppSettings<"app.config">
 
-let episodesUrl = "http://en.wikipedia.org/wiki/List_of_The_Simpsons_episodes"
+let dataDirectory = "F:\Repos\SimpsonsEpisodes\Data"
+let episodeUrlStart = "http://en.wikipedia.org/wiki/The_Simpsons_(season_"
+let currentNumberOfSeries = 27
 
+let getSeasonFileName = fun (seasonNumber: int) -> 
+    Path.Combine( dataDirectory, "Season_" + seasonNumber.ToString() + ".html")
 
 [<EntryPoint>]
 let main argv = 
-    let a  = 0
-    let episodes = HtmlDocument.Load(episodesUrl)
-    let trElements = episodes.Descendants["tr"]
-    let episodeRows: seq<HtmlNode> = 
-        trElements
-        |> Seq.filter(fun (e: HtmlNode) -> 
-            e.HasAttribute("class", "vevent")
-        )
+    // Download files to disk
+    for seriesNumber in 1 .. currentNumberOfSeries do
+        if File.Exists(getSeasonFileName(seriesNumber)) then
+            Console.WriteLine("File exists at:" + getSeasonFileName(seriesNumber))
+        else
+            let seasonUrl = episodeUrlStart + seriesNumber.ToString() + ")"
+            Console.WriteLine("Url for season is: " + seasonUrl)
+            let seasonHtml = HtmlDocument.Load(seasonUrl)
+            File.WriteAllLines(getSeasonFileName(seriesNumber), [seasonHtml.ToString()])
+            Console.WriteLine("Downloaded to file: " + getSeasonFileName(seriesNumber))
+//    let episodes = HtmlDocument.Load(episodesUrl)
+//    let trElements = episodes.Descendants["tr"]
+//    // For some reason the episodes are in table rows with a 'vevent' class
+//    let episodeRowClass = "vevent"
+//    let episodeRows: seq<HtmlNode> = 
+//        trElements
+//        |> Seq.filter(fun (e: HtmlNode) -> 
+//            e.HasAttribute("class", episodeRowClass) )
 
     Console.ReadKey() |> ignore
     0 // return an integer exit code
