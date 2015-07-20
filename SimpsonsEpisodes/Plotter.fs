ef6e823eb2e92109c523f80490c8c5f198a26aba
Chester Burbidge <chester.burbidge@gmail.com>
2015-07-20 23:50:02 +0100

    Added plotting

--- SimpsonsEpisodes/Plotter.fs
+++ SimpsonsEpisodes/Plotter.fs
@@ -5,22 +5,45 @@ open RDotNet
 open RProvider
 open RProvider.graphics
 
+let getRatios(filmography:SimpsonsFilmography, getResult) =
+    filmography.seasons
+    |> List.map getResult
+    |> List.map (fun x -> (double)x)
 
+let plotLine(seasonNumbers, values, colour:string) =
+    namedParams [
+        "x", box seasonNumbers;
+        "y", box values;
+        "type", box "o";
+        "col", box colour;
+    ]
+    |> R.lines
+    |> ignore
 
 let plot (filmography: SimpsonsFilmography) =
-    //R.plot()
     
-    let widgets = [ 3; 8; 12; 15; 19; 18; 18; 20; ]
-    let sprockets = [ 5; 4; 6; 7; 12; 9; 5; 6; ]
+    let seasonNumbers = filmography.seasons
+                        |> List.map (fun x -> x.seasonNumber)
 
-    R.plot(widgets)
+    let maggieRatios = getRatios(filmography, (fun x -> x.result.Maggie.frequency))
+    let lisaRatios = getRatios(filmography, (fun x -> x.result.Lisa.frequency))
+    let bartRatios = getRatios(filmography, (fun x -> x.result.Bart.frequency))
+    let margeRatios = getRatios(filmography, (fun x -> x.result.Marge.frequency))
+    let homerRatios = getRatios(filmography, (fun x -> x.result.Homer.frequency))
 
-    R.plot(widgets, sprockets)
-
-    R.barplot(widgets)
-
-    R.hist(sprockets)
-
-    R.pie(widgets)
-
+    let biggestValue: double = List.concat ([maggieRatios; lisaRatios; bartRatios; margeRatios; homerRatios ])
+                               |> List.max
+    
+    namedParams [   
+        "x", box seasonNumbers; 
+        "ylim", box [0.0; biggestValue] 
+        ]
+    |> R.plot
+    |> ignore
+    
+    plotLine(seasonNumbers, maggieRatios, "red")
+    plotLine(seasonNumbers, lisaRatios, "blue")
+    plotLine(seasonNumbers, bartRatios, "green")
+    plotLine(seasonNumbers, margeRatios, "yellow")
+    plotLine(seasonNumbers, homerRatios, "black")
     0
--- SimpsonsEpisodes/Program.fs
+++ SimpsonsEpisodes/Program.fs
@@ -7,7 +7,9 @@ let main argv =
     let episodes = SimpsonsEpisodes.Parser.getEpisodes
     
     let results = SimpsonsEpisodes.Analyser.analyseEpisodes episodes
-    
+//    let seasonOne = SimpsonsEpisodes.Analyser.SimpsonsSeason(1, [SimpsonsEpisodes.Analyser.SimpsonsEpisode(1, 1, "a ", "bart", "")])
+//    let seasonTwo = SimpsonsEpisodes.Analyser.SimpsonsSeason(2, [SimpsonsEpisodes.Analyser.SimpsonsEpisode(2, 1, "a ", "bart", "bart")])
+//    let results = SimpsonsEpisodes.Analyser.SimpsonsFilmography([seasonOne; seasonTwo])
     SimpsonsEpisodes.Plotter.plot results |> ignore
     
     Console.WriteLine("")
